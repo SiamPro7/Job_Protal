b4802fb56b98423c66a86eaa86f24df4a89f0279
siampro7 <dynamicsiam7@gmail.com>
2024-12-18 12:42:25 +0600

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from './Banner';
+import HotJobs from './HotJobs';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <HotJobs/>
+        </div>
+    );
+};
+
+export default Home;
